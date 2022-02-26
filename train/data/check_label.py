805f8f743484af53b65151261555babcfac8dbc0
hojoonson <joonyyyy@gmail.com>
2022-02-26 23:59:57 +0900

    update chek_label.py

--- train/data/check_label.py
+++ train/data/check_label.py
@@ -1,12 +1,13 @@
-f = open("scherule_trainlabels.txt","r")
-data=f.readlines()
-truecount=0
-falsecount=0
-for element in data:
-    if element.split(" ")[1]=="0":
-        falsecount+=1
-    elif element.split(" ")[1]=="1":
-        truecount+=1
-    print("True : "+str(truecount)+" False : "+str(falsecount))
-f.close()
+with open('./train/trainlabels/Scherule_trainlabels.txt','r') as f:
+    data=f.readlines()
+    truecount=0
+    falsecount=0
+    for element in data:
+        splited = element.split(' ')
+        label = splited[3]
+        if label=='0':
+            falsecount+=1
+        elif label=='1':
+            truecount+=1
+    print(f'True : {truecount}, False : {falsecount}')
 
