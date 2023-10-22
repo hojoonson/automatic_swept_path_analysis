b6a2b0ae9c4b74b4b28cd12daa1d7e6e038025e5
hojoonson <joonyyyy@gmail.com>
2023-10-21 21:46:47 -0400

    remove nvidia-docker

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 xhost +
-nvidia-docker run -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/projects/automatic_swept_path_analysis:/app pygame_tf
+docker run --gpus all -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/projects/automatic_swept_path_analysis:/app pygame_tf
